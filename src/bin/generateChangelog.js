1f9d6baab9d742dba70452ec6bbda4e10e4758a5
kaliabadi <kouros_aliabadi@hotmail.com>
2018-04-18 11:12:31 +0100

    feature: bumping version number and generating changelog in code

---
+++ src/bin/generateChangelog.js
@@ -0,0 +1,16 @@
+import mversion from 'mversion';
+import changeLog from 'generate-changelog'
+import fs from 'fs';
+
+const generateChangeLog = (version) => {
+    mversion.update(version, function (err, data) { });
+
+    changeLog.generate({ [version]: true })
+        .then(function (changelog) {
+            fs.writeFileSync('./CHANGELOG.md', changelog);
+        });
+};
+
+module.exports = {
+    generateChangeLog
+}
