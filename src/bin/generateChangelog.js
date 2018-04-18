d9023c4ebf0d098aa09b339573201a072d2b9758
kaliabadi <kouros_aliabadi@hotmail.com>
2018-04-18 16:48:58 +0100

    fix: using tags in changelog generation

--- src/bin/generateChangelog.js
+++ src/bin/generateChangelog.js
@@ -1,9 +1,14 @@
 import changeLog from 'generate-changelog'
 import fs from 'fs';
+import gitTags from 'git-tags';
 
 export default () => {
-    changeLog.generate({ patch: false })
+    gitTags.get((err, tags) => {
+        const versionRange = tags[1] ? `${tags[1]}..${tags[0]}` : tags[0]; 
+        
+        changeLog.generate({ patch: false }, { tag: versionRange})
         .then((changelog) => {
             fs.writeFileSync('./CHANGELOG.md', changelog);
         });
+    })
 };
