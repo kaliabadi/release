c61eb425691f6e93a424cdd1a05ce16c3fbcf974
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-04-18 14:16:43 +0100

    Merged with master.

---
+++ src/bin/generateChangelog.js
@@ -0,0 +1,11 @@
+import mversion from 'mversion';
+import changeLog from 'generate-changelog'
+import fs from 'fs';
+
+export default (version) => {
+    mversion.update(version, () => {});
+
+    changeLog.generate({[version]: true}).then((changelog) => {
+        fs.writeFileSync('./CHANGELOG.md', changelog);
+    });
+};
