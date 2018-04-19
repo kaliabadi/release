9b9b757e57ceb643046249c93355da0fbe6b92ae
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-04-19 16:02:13 +0100

    test: update git interactions test

--- src/bin/gitInteractions.test.js
+++ src/bin/gitInteractions.test.js
@@ -1,6 +1,7 @@
 import sinon from 'sinon';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import remoteOriginUrl from 'remote-origin-url';
 import {latestRelease} from './gitInteractions';
 import GithubApi from './api/GithubApi';
 
@@ -9,9 +10,15 @@ chai.use(sinonChai);
 describe('gitInteractions', () => {
 
     const userDetails = { username: 'tools', accessToken: 'hammer'};
+    const orgRepo = 'newsuk/release';
     var sandbox;
 
-    beforeEach(() => sandbox = sinon.sandbox.create());
+    beforeEach(() => {
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(remoteOriginUrl, 'sync')
+            .returns(`git@github.com:${orgRepo}.git`);
+    });
+
     afterEach(() => sandbox.restore());
 
     describe('latestRelease', () => {
@@ -19,13 +26,11 @@ describe('gitInteractions', () => {
         it('should return the latest release', async () => {
             const expectedVersion = 'v1.0.0';
             const apiStub = sandbox.stub(GithubApi.prototype, 'latestRelease').resolves(expectedVersion);
-            const repo = 'release';
-            const org = 'newsuk';
 
-            const releaseVersion = await latestRelease(userDetails, {repo, org});
+            const releaseVersion = await latestRelease(userDetails);
 
             releaseVersion.should.equal(expectedVersion);
-            apiStub.should.have.been.calledWith(org, repo);
+            apiStub.should.have.been.calledWith(orgRepo);
         });
 
     });
