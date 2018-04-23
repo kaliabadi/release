0d97b251aa122aad0f8cfa042aa0887d6eb4b932
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-04-23 17:30:22 +0100

    test: latestReleaseAction

--- src/bin/actions/latestReleaseAction.js
+++ src/bin/actions/latestReleaseAction.js
@@ -1,9 +1,12 @@
 import {latestRelease, getOrgRepo} from '../github/gitInteractions';
 
-
 export default async (userDetails) => {
-  const repoDetails = getOrgRepo();  
+  const repoDetails = getOrgRepo();
   const latestReleaseResponse = await latestRelease(userDetails);
 
-  console.log(`The latest release version for ${repoDetails} is ${latestReleaseResponse.name}`);
+  if (latestReleaseResponse.error) {
+    console.error('Failed to get the latest release version', latestReleaseResponse.error);
+  } else {
+    console.log(`The latest release version for ${repoDetails} is ${latestReleaseResponse.name}`);
+  }
 };
