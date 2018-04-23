48e795847d73de076cc1b86e21f8e2e0defa8baf
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-04-23 15:16:43 +0100

    test: GithubApi - updateRelease

--- src/bin/api/GithubApi.test.js
+++ src/bin/api/GithubApi.test.js
@@ -8,22 +8,30 @@ chai.use(sinonChai);
 
 describe('GithubApi', () => {
 
-    var sandbox;
     const userDetails = {
         username: 'tools',
         accessToken: 'hammer'
     };
     const repoDetails = 'tools/release';
-    const goodSendStub = (body) => sinon.stub().returns(({body}));
-    const goodSetStub = (body) => sinon.stub().returns({send: goodSendStub(body)});
-    const goodRequestMock = (body) => ({set: goodSetStub(body)});
-    const badSendStub = sinon.stub().rejects({response: {body: 'Github Error'}});
-    const badSetStub = sinon.stub().returns({send: badSendStub});
-    const badRequestMock = () => ({set: badSetStub});
+    let sandbox;
+    let api;
+    let goodSendStub;
+    let goodSetStub;
+    let goodRequestMock;
+    let badSendStub;
+    let badSetStub;
+    let badRequestMock;
 
     beforeEach(() => {
         sandbox = sinon.sandbox.create();
+        api = new GithubApi(userDetails);
         sandbox.stub(console, "error");
+        goodSendStub = (body) => sandbox.stub().returns(({body}));
+        goodSetStub = (body) => sandbox.stub().returns({send: goodSendStub(body)});
+        goodRequestMock = (body) => ({set: goodSetStub(body)});
+        badSendStub = sandbox.stub().rejects({response: {body: 'Github Error'}});
+        badSetStub = sandbox.stub().returns({send: badSendStub});
+        badRequestMock = () => ({set: badSetStub});
     });
 
     afterEach(() => sandbox.restore());
@@ -32,7 +40,6 @@ describe('GithubApi', () => {
 
         it('should return a valid response', async () => {
             // Setup.
-            const api = new GithubApi(userDetails);
             const expectedBody = {name: '1.2.3'};
             sandbox.stub(request, 'get').returns(goodRequestMock(expectedBody));
 
@@ -47,7 +54,6 @@ describe('GithubApi', () => {
 
         it('should return an error', async () => {
             // Setup.
-            const api = new GithubApi(userDetails);
             sandbox.stub(request, 'get').returns(badRequestMock());
 
             // Exercise.
@@ -63,7 +69,6 @@ describe('GithubApi', () => {
 
         it('should return a valid response', async () => {
             // Setup.
-            const api = new GithubApi(userDetails);
             const expectedBody = {id: '1.2.3'};
             const expectedTag = '1.2.3';
             sandbox.stub(request, 'get').returns(goodRequestMock(expectedBody));
@@ -79,11 +84,42 @@ describe('GithubApi', () => {
 
         it('should return an error', async () => {
             // Setup.
-            const api = new GithubApi(userDetails);
             sandbox.stub(request, 'get').returns(badRequestMock());
 
             // Exercise.
             const response = await api.taggedRelease(repoDetails, '1.2.3');
+
+            // Verify.
+            should.exist(response.error);
+        });
+
+    });
+
+    describe('updateRelease', () => {
+
+        it('should return a valid response', async () => {
+            // Setup.
+            const expectedBody = {name: '1.2.3'};
+            const expectedReleaseId = '10669493';
+            const expectedReleaseDetails = '*release content details*'
+            sandbox.stub(request, 'patch').returns(goodRequestMock(expectedBody));
+
+            // Exercise.
+            const response = await api.updateRelease(repoDetails, expectedReleaseId, expectedReleaseDetails);
+
+            // Verify.
+            request.patch.should.be.calledWith(`https://api.github.com/repos/${repoDetails}/releases/${expectedReleaseId}`);
+            request.patch().set.should.be.calledWith('Authorization', 'Basic dG9vbHM6aGFtbWVy');
+            request.patch().set().send.should.be.calledWith(expectedReleaseDetails);
+            response.should.deep.equal(expectedBody);
+        });
+
+        it('should return an error', async () => {
+            // Setup.
+            sandbox.stub(request, 'patch').returns(badRequestMock());
+
+            // Exercise.
+            const response = await api.updateRelease(repoDetails, '10669493', '*release content details*');
 
             // Verify.
             should.exist(response.error);
